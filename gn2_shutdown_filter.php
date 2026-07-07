929d94470680a03af619d318d38ceec0754f6305
Ingo Winter <iw@gn2.de>
2026-07-07 15:04:49 +0200

    25-17873 fix(shutdown): WP 6.9.0 Enhancement Buffer Kompatibilität
    ob_get_clean() umging den WP 6.9.0 ob-Callback (wp_finalize_template_enhancement_output_buffer),
    wodurch späte Styles (leaflet-css, Block-Styles) nicht in <head> gehoisted wurden.
    
    Fix: Innere Buffer mit ob_end_flush() schließen (Callback läuft), dann erst ob_get_clean()
    auf gn2s eigenen Buffer anwenden.

--- gn2_shutdown_filter.php
+++ gn2_shutdown_filter.php
@@ -3,7 +3,7 @@
 Plugin Name:  gn2 Shutdown Filter
 Plugin URI:   https://github.com/gn2netwerk/gn2_shutdown_filter
 Description:  Provides the custom filter "gn2_shutdown"
-Version:      1.0.1
+Version:      1.0.2
 Author:       gn2
 Author URI:   https://www.gn2.de/
 Update URI:   https://raw.githubusercontent.com/gn2netwerk/gn2_shutdown_filter/master/info.json
@@ -26,14 +26,20 @@ add_filter('update_plugins_raw.githubusercontent.com', function ($update, $plugi
     return $json;
 }, 10, 3);
 
+// Level vor ob_start() merken, damit der Shutdown-Handler genau weiß, welcher Buffer gn2 gehört.
+// Nötig seit WP 6.9.0: wp_start_template_enhancement_output_buffer() öffnet einen eigenen Buffer
+// mit Callback (wp_finalize_template_enhancement_output_buffer), der Styles in <head> hoistet.
+// Der frühere ob_get_clean()-Loop hat diesen Callback umgangen → leaflet-css und Block-Styles fehlten.
+$gn2_ob_level = ob_get_level();
 ob_start();
-add_action('shutdown', function () {
-    $html = '';
-    // alles an output einsammeln
-    $levels = ob_get_level();
-    for ($i = 0; $i < $levels; $i++) {
-        $html .= ob_get_clean();
+add_action('shutdown', function () use ($gn2_ob_level) {
+    // Alle Buffer OBERHALB von gn2 mit ob_end_flush() schließen (nicht ob_get_clean!).
+    // ob_end_flush() führt den Buffer-Callback aus → WP Enhancement Buffer hoistet Styles korrekt,
+    // der Inhalt fließt dann in gn2s eigenen Buffer.
+    while (ob_get_level() > $gn2_ob_level + 1) {
+        ob_end_flush();
     }
-    // filter registrieren
+    // Jetzt gn2s eigenen Buffer abgreifen – enthält fertiges HTML mit geholisteten Styles.
+    $html = ob_get_clean() ?? '';
     echo apply_filters('gn2_shutdown', $html);
 }, 0);
